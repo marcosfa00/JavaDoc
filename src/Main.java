b046f3c6648f6a76fb0684b569f5508f01608f77
Marcos F. Avendaño <marcosfavendano@gmail.com>
2022-11-23 16:43:32 +0100

    elimino un sout()

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
      * @param args argumentos
      */
     public static void main(String[] args) {
-        System.out.println("CIRCULO");
+
         String str_circun = "la circunferencia es de : ";
         String str_centre = "El centro del circulo es: ";
         Circulo c1 = new Circulo(0.5,4,7);
@@ -17,6 +17,7 @@ public class Main {
         System.out.println(str_centre + c1.getCentroX());
         c1.escala(7);
         System.out.println(str_circun + c1.getCircunferencia());
+
 
 
 
